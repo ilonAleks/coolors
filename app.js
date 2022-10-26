877cfd16b91f9b5373650858103eada512d039c6
ilonAleks <106811359+ilonAleks@users.noreply.github.com>
2022-10-26 15:59:57 +0300

    Delete comments

--- app.js
+++ app.js
@@ -24,11 +24,6 @@ document.addEventListener('click', (event) => {
 })
 
 function gerenerateRandomColor() {
-  // RGB
-  // #FF0000
-  // #00FF00
-  // #0000FF
-
   const hexCodes = '0123456789ABCDEF'
   let color = ''
   for (let i = 0; i < 6; i++) {
